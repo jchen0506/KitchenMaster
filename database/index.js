75085a3d75c798612b6d9e126a41b38e719939c8
Jing Chen <jchen0506@gmail.com>
2020-07-27 22:14:21 -0500

    heroku redis setup

--- database/index.js
+++ database/index.js
@@ -1,5 +1,5 @@
 const redis = require('redis');
-const client = redis.createClient();
+const client = redis.createClient(process.env.REDIS_URL);
 
 client.on('error', function (error) {
   console.error(error);
